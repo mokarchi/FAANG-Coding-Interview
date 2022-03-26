0509697590e1289c17ae999e4c7545e2ce9fb94f
Amir <mokarchi@gmail.com>
2022-03-26 10:21:16 +0430

    Update TwoSum.cs

--- Algorithms/TwoSum.cs
+++ Algorithms/TwoSum.cs
@@ -1,4 +1,5 @@
 public class Solution {
+    // Time Complexity: O(n^2), Space complexity: O(1)
     public int[] TwoSum(int[] nums, int target) {
         int numberToFind = 0;
         for (int p1 = 0; p1 < nums.Length; p1++)
@@ -13,5 +14,20 @@ public class Solution {
             }
         }
         return new int[]{};
+    }
+    
+    // Time Complexity: O(n), Space complexity: O(n)
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int, int> numsMap = new Dictionary<int, int>();
+        for (int p = 0; p < nums.Length; p++)
+        {
+            if (numsMap.TryGetValue(target - nums[p], out int index))
+            {
+                return new[] { index, p };
+            }
+            
+            numsMap[nums[p]] = p;
+        }
+        return new int[] { };
     }
 }
